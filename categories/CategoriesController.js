74ff9985f9680223ff322d3318437ae90c931254
Rafael Ferreira Lemes <40215566+rflfl@users.noreply.github.com>
2023-08-11 13:08:26 +0000

    feat: criados controllers CategoriesController e ArticlesController

---
+++ categories/CategoriesController.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+
+router.get("/admin/categories/new", (req,res) => {
+    res.render("admin/categories/new")
+})
+
+
+module.exports = router
